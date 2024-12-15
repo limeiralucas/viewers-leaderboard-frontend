552b1f9b5f45f94fd61183df9978db5e283bc0a8
Lucas Limeira <lucasalveslm@gmail.com>
2024-12-15 16:35:30 -0300

    refactor: removed console log

--- src/api/viewersRanking.ts
+++ src/api/viewersRanking.ts
@@ -3,7 +3,5 @@ import { apiClient } from "./client";
 export async function getViewersRanking(channelId: string) {
   const response = await apiClient.get<ViewersRanking>(`/ranking/${channelId}`);
 
-  console.log(response.data);
-
   return response.data;
 }
